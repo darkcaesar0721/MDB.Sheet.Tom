45ef20e0f8d4e136a782e7e4b1895fec4072c359
darkcaesar0721 <darkcaesar0721@gmail.com>
2023-08-31 04:12:34 +0900

    - create mongodb server and client

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+
+/* BEGIN: Create a mongodb server and mongodb client */
+require('./database/server');
+require('./database/client');
+/* END: Create a mongodb server and mongodb client */
+
 const express = require('express');
 const app = express();
 
