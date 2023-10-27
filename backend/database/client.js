171349d3ed2de72e7f0c049f767af949b1f67daf
darkcaesar0721 <darkcaesar0721@gmail.com>
2023-10-27 21:29:21 +0900

    - database client remove comment

--- backend/database/client.js
+++ backend/database/client.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
 
-// Connection URL
 mongoose.Promise = global.Promise
 
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
